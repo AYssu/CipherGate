9aae6bd7ee1e3a14727eb3fadc051392b9ef4ed6
AYssu <2997036064@qq.com>
2026-03-23 23:32:36 +0800

    修复head区域的问题

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -320,7 +320,8 @@ const MainLayout: React.FC = () => {
           borderBottom: '1px solid #f0f0f0',
           display: 'flex',
           justifyContent: 'space-between',
-          alignItems: 'center'
+          alignItems: 'center',
+          flexDirection: 'row'
         }}>
           <Space size={12} align="center">
             <Button
