fc4684efed01ff00eecd4fe6e8aa06dda3252b4b
AYssu <2997036064@qq.com>
2026-06-26 18:14:11 +0800

    feat: add documentation mapper interfaces

---
+++ src/main/java/com/ayssu/ciphergate/doc/mapper/DocCategoryMapper.java
@@ -0,0 +1,9 @@
+package com.ayssu.ciphergate.doc.mapper;
+
+import com.ayssu.ciphergate.doc.entity.DocCategory;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DocCategoryMapper extends BaseMapper<DocCategory> {
+}
---
+++ src/main/java/com/ayssu/ciphergate/doc/mapper/DocItemMapper.java
@@ -0,0 +1,9 @@
+package com.ayssu.ciphergate.doc.mapper;
+
+import com.ayssu.ciphergate.doc.entity.DocItem;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DocItemMapper extends BaseMapper<DocItem> {
+}
